fcbda1df6023ea8c2f25ce87fc1ccd9225547aad
creadorwebline <creadwebline@gmail.com>
2020-12-10 14:17:44 -0500

    Andres
    trash
    
    Co-Authored-By: Asantibo <42550963+asantibo@users.noreply.github.com>

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
 <head>
     <?php
     include('header.html');
-    include("sendemail.php");
     ?>
 </head>
 <!--/head-->
--- menus.php
+++
@@ -1,2 +0,0 @@
-<?php
-header("Location: menus-webline/info/info-menus.php");
