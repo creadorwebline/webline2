60d0652ac3a20f37ee21d05661badb0b4a38635d
David2813 <quinterosotodavidjavier@gmail.com>
2020-12-13 15:05:34 -0500

    David Quintero
    Mejoras del mensaje de error

--- clientes/model/iglesia-san-norberto/logicaVariada/EnvioDeMensaje.php
+++ clientes/model/iglesia-san-norberto/logicaVariada/EnvioDeMensaje.php
@@ -1,7 +1,32 @@
 <?php
 class EnvioDeMensaje{
-    function mensaje($mesaje,$pagina){
-        header("Refresh: 0; URL=".$pagina);
-        echo "<script>alert('$mesaje');</script>";
+    function mensaje($mensajeM,$pagina){
+        ?>
+    <html>
+    <head> 
+        <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+        <script type="text/javascript">
+        function imprimir(impresion,pagina){
+            swal({
+                title:impresion,icon :"info"}).then((value) => {
+                    window.location = '<?php echo $pagina; ?>';
+                ;
+                });
+        }
+        </script>
+    </head>
+    <body>
+             <?php
+            ?><script LANGUAGE="JavaScript">
+            imprimir('<?php echo $mensajeM; ?>','<?php echo $pagina; ?>');</script>
+            <?php
+            /*
+            codigo de respaldo
+            //header("Refresh: 0; URL=".$pagina);
+            //echo "<script>alert(".$mensajeM.");</script>";
+            */ 
     }
 }
+?>
+    </body>
+</html>
